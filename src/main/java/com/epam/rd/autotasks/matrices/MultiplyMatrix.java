6500f6b97528db39497ca7567c7aad5d13cbbea4
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-07-09 19:25:26 +0000

    3

--- src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
+++ src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
@@ -4,35 +4,21 @@ import java.util.Arrays;
 public class MultiplyMatrix {
     public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
 
-       int[][] res = new int[matrix1.length][matrix2.length];
-        int i, j, k;
-        for (i = 0; i < matrix1.length; i++) {
-            for (j = 0; j < matrix2.length; j++) {
-                res[i][j] = 0;
-                for (k = 0; k < matrix2.length; k++)
-                    res[i][j] += matrix1[i][k]
-                            * matrix2[k][j];
+        int[][] result = new int[matrix1.length][matrix2[0].length];
+
+        for (int row = 0; row < result.length; row++) {
+            for (int col = 0; col < result[row].length; col++) {
+                result[row][col] = multiplyMatricesCell(matrix1, matrix2, row, col);
             }
         }
-        return res;
+        return result;
     }
 
-    // public static void main(String[] args) {
-
-    //     System.out.println("Test your code here!\n");
-
-    //     // Get a result of your code
-
-    //     int[][] a = {
-    //             {0, 12345},
-    //             {4509, 0},
-    //             {3, 567} };
-
-    //     int[][] b = {
-    //             {653, 0, 25353},
-    //             {0, 61, 6} };
-
-    //     int[][] result = multiply(a, b);
-    //     System.out.println(Arrays.deepToString(result).replace("],", "]\n"));
-    // }
+    public static int multiplyMatricesCell(int[][] matrix1, int[][] matrix2, int row, int col) {
+        int cell = 0;
+        for (int i = 0; i < matrix2.length; i++) {
+            cell += matrix1[row][i] * matrix2[i][col];
+        }
+        return cell;
+    }
 }
